3f169c339f78c03fcef840f5005e0f1981dc1275
clapsheep <sktndid1203@naver.com>
2024-07-25 00:01:35 +0900

    solved SWEA 1926

---
+++ src/swea/SWEA1926.java
@@ -0,0 +1,33 @@
+package swea;
+
+import java.util.Scanner;
+
+public class SWEA1926 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for (int n = 1; n <= N; n++) {
+			String res = "";
+			int a = n%10; // 일의자리
+			int b = n/10; //십의자리
+			int c = n/100; // 백의 자리
+			
+			if(a==3||a==6||a==9) {
+				res+="-";
+			}
+			if(b==3||b==6||b==9) {
+				res+="-";
+			}
+			if(c==3||c==6||c==9) {
+				res+="-";
+			}
+			if(res.length() ==0) {
+				System.out.print(n+" ");
+			}else {
+				System.out.print(res+ " ");
+			}
+
+		}
+	}
+	
+}
