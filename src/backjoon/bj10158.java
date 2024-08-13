f278ee8e8552a3af20482c8de266962adfcc98f2
clapsheep <sktndid1203@naver.com>
2024-08-13 18:42:55 +0900

    asd

---
+++ src/backjoon/bj10158.java
@@ -0,0 +1,18 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj10158 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st= new StringTokenizer(br.readLine());
+		int w = Integer.parseInt(st.nextToken());
+		int h = Integer.parseInt(st.nextToken());
+		int[][] map = new int[h][w];
+		
+	}
+
+}
