988dde3054de0718d58504340eb7d4c3eaeac4d1
clapsheep <sktndid1203@naver.com>
2024-08-14 17:59:25 +0900

    0814

--- src/backjoon/bj10158.java
+++ src/backjoon/bj10158.java
@@ -8,11 +8,19 @@ public class bj10158 {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st= new StringTokenizer(br.readLine());
-		int w = Integer.parseInt(st.nextToken());
-		int h = Integer.parseInt(st.nextToken());
-		int[][] map = new int[h][w];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int initX = Integer.parseInt(st.nextToken());
+		int initY = Integer.parseInt(st.nextToken());
+		int T = Integer.parseInt(br.readLine());
+
+		int endX = x-Math.abs(x-(initX+T)%(2*x));
+		int endY = y-Math.abs(y-(initY+T)%(2*y));
 		
+		System.out.printf("%d %d" ,endX,endY);
+
 	}
 
 }
