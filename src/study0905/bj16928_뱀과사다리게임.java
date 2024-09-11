701c21026b302b217b7438e5b1ad21f8606bc7ce
clapsheep <sktndid1203@naver.com>
2024-09-12 01:23:09 +0900

    고마해라 마이 했다 아이가

--- src/study0905/bj16928_뱀과사다리게임.java
+++ src/study0905/bj16928_뱀과사다리게임.java
@@ -3,6 +3,7 @@ package study0905;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -36,40 +37,38 @@ public class bj16928_뱀과사다리게임 {
 			int e = Integer.parseInt(st.nextToken());
 			snake[s - 1] = e - 1;
 		}
-		int res = 10100000;
-		dfs(0,0);
-		System.out.println(res);
-		
+//		res = 10100000;
+//		dfs(0, 0);
+//		System.out.println(res);
+		int ans = bfs();
+		System.out.println(ans);
+
 	}
 
-	private static void dfs(int cur, int sum) {
-		if(sum>res)return;
-		if(cur == 99) {
-			res= Math.min(res, sum);
+	private static void dfs(int cur, int cnt) {
+		if (cnt > res)
+			return;
+		if (cur == 99) {
+			res = Math.min(res, cnt);
 			return;
 		}
 		for (int i = 1; i <= 6; i++) {
-			int next = cur+i;
+			int next = cur + i;
 			if (next > 99)
 				continue;
 			if (v[next])
 				continue;
-			if(ladder[next]!=0) {
-				v[ladder[next]] = true;
-				dfs(ladder[next], sum+1);
-				v[ladder[next]] = false;
-			}else if(snake[next]!=0){
-				v[snake[next]] = true;
-				dfs(snake[next], sum+1);
-				v[snake[next]] = false;
-			}else{
-				v[next] = true;
-				dfs(cur+1,sum+1);
-				v[next] = false;
+			v[next] = true;
+			if (ladder[next] != 0) {
+				dfs(ladder[next], cnt + 1);
+			} else if (snake[next] != 0) {
+				dfs(snake[next], cnt + 1);
+			} else {
+				dfs(next, cnt + 1);
 			}
-			
+			v[next] = false;
 		}
-		
+
 	}
 
 	private static int bfs() {
@@ -78,28 +77,31 @@ public class bj16928_뱀과사다리게임 {
 		v[0] = true;
 		int cnt = 0;
 		while (!q.isEmpty()) {
-			int cur = q.poll();
-			if (cur == 99)
-				return cnt;
-			cnt++;
-			for (int i = 1; i <= 6; i++) {
-				int next = cur + i;
-				if (next > 99)
-					continue;
-				if (v[next])
-					continue;
-				if (ladder[next] != 0) {
-					q.add(ladder[next]);
-					v[ladder[next]] = true;
-				} else if (snake[next] != 0) {
-					q.add(snake[next]);
-					v[snake[next]] = true;
-				} else {
-					q.add(next);
-					v[next] = true;
+			
+			int size = q.size();
+			for (int s = 0; s < size; s++) {
+				int cur = q.poll();
+				if (cur == 99)
+					return cnt;
+				for (int i = 1; i <= 6; i++) {
+					int next = cur + i;
+					if (next > 99)
+						continue;
+					if (v[next])
+						continue;
+					if (ladder[next] != 0) {
+						q.add(ladder[next]);
+						v[ladder[next]] = true;
+					} else if (snake[next] != 0) {
+						q.add(snake[next]);
+						v[snake[next]] = true;
+					} else {
+						q.add(next);
+						v[next] = true;
+					}
 				}
 			}
-
+			cnt++;
 		}
 		return -1;
 	}
