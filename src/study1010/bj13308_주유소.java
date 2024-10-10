e6d72e59e4d5b66a7d46fadf7911089f2536ab38
clapsheep <sktndid1203@naver.com>
2024-10-10 17:51:13 +0900

    123

--- src/study1010/bj13308_주유소.java
+++ src/study1010/bj13308_주유소.java
@@ -2,15 +2,54 @@ package study1010;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class bj13308_주유소 {
-	static int N,M;
+	static int N, M;
+	static int[] fuel;
+
+	static class Node {
+		int v, w;
+
+		public Node(int v, int w) {
+			this.v = v;
+			this.w = w;
+		}
+	}
+
+	static final int INF = Integer.MAX_VALUE;
+	static List<Node>[] adj;
+	static int[] dist;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
+		fuel = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			fuel[i] = Integer.parseInt(st.nextToken());
+		}
+		adj = new ArrayList[N];
+		for (int i = 0; i < N; i++) {
+			adj[i] = new ArrayList<>();
+		}
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken()) - 1;
+			int b = Integer.parseInt(st.nextToken()) - 1;
+			int w = Integer.parseInt(st.nextToken());
+			adj[a].add(new Node(b,w));
+			adj[b].add(new Node(a,w));
+		}
+		
+		dist= new int[N];
+		for (int i = 0; i < N; i++) {
+			dist[i] = INF;
+		}
 
 	}
 
